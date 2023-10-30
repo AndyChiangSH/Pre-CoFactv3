ddcdb732d9b921697739c0f4b341a54a695ff95c
AndyChiangSH <andy10801@gmail.com>
2023-10-30 20:47:28 +0800

    First train (claim text & evidence text)

--- model.py
+++ model.py
@@ -55,7 +55,7 @@ class FakeNet(nn.Module):
         self.claim_document_image_text_pos_ffn = PositionwiseFeedForward(config['hidden_dim'], config['hidden_dim']*2, dropout=config['dropout'])
 
         self.attention_fusion = nn.Sequential(
-            nn.Linear(config['hidden_dim']*16, config['hidden_dim']),
+            nn.Linear(config['hidden_dim']*4, config['hidden_dim']),
             nn.ReLU(),
         )
 
@@ -65,17 +65,17 @@ class FakeNet(nn.Module):
         )
 
         self.classifier = nn.Sequential(
-            nn.Linear(16+config['hidden_dim'], 128),
+            nn.Linear(config['hidden_dim'], 128),
             nn.ReLU(),
-            nn.Linear(128, 5)
+            nn.Linear(128, 3)
         )
 
-    def forward(self, claim_text, claim_image, document_text, document_image, add_feature):
+    def forward(self, claim_text, document_text):
         # transform to embeddings
         claim_text_embedding = self.text_embedding(claim_text)
-        claim_image_embedding = self.image_embedding(claim_image)
+        # claim_image_embedding = self.image_embedding(claim_image)
         document_text_embedding = self.document_text_embedding(document_text)
-        document_image_embedding = self.document_image_embedding(document_image)
+        # document_image_embedding = self.document_image_embedding(document_image)
 
         # claim-document attention
         claim_document_text, _ = self.claim_document_text_attention(claim_text_embedding, document_text_embedding, document_text_embedding)
@@ -83,66 +83,64 @@ class FakeNet(nn.Module):
         document_claim_text, _ = self.claim_document_text_attention(document_text_embedding, claim_text_embedding, claim_text_embedding)
         document_claim_text = self.claim_document_text_pos_ffn(document_claim_text)
 
-        claim_document_image, _ = self.claim_document_image_attention(claim_image_embedding, document_image_embedding, document_image_embedding)
-        claim_document_image = self.claim_document_image_pos_ffn(claim_document_image)
-        document_claim_image, _ = self.claim_document_image_attention(document_image_embedding, claim_image_embedding, claim_image_embedding)
-        document_claim_image = self.claim_document_image_pos_ffn(document_claim_image)
+        # claim_document_image, _ = self.claim_document_image_attention(claim_image_embedding, document_image_embedding, document_image_embedding)
+        # claim_document_image = self.claim_document_image_pos_ffn(claim_document_image)
+        # document_claim_image, _ = self.claim_document_image_attention(document_image_embedding, claim_image_embedding, claim_image_embedding)
+        # document_claim_image = self.claim_document_image_pos_ffn(document_claim_image)
 
         # text-image co-attention
-        claim_text_image, _ = self.text_image_attention(claim_text_embedding, claim_image_embedding, claim_image_embedding)
-        claim_text_image = self.text_image_pos_ffn(claim_text_image)
-        claim_image_text, _ = self.image_text_attention(claim_image_embedding, claim_text_embedding, claim_text_embedding)
-        claim_image_text = self.image_text_pos_ffn(claim_image_text)
+        # claim_text_image, _ = self.text_image_attention(claim_text_embedding, claim_image_embedding, claim_image_embedding)
+        # claim_text_image = self.text_image_pos_ffn(claim_text_image)
+        # claim_image_text, _ = self.image_text_attention(claim_image_embedding, claim_text_embedding, claim_text_embedding)
+        # claim_image_text = self.image_text_pos_ffn(claim_image_text)
 
-        document_text_image, _ = self.text_image_attention(document_text_embedding, document_image_embedding, document_image_embedding)
-        document_text_image = self.text_image_pos_ffn(document_text_image)
-        document_image_text, _ = self.image_text_attention(document_image_embedding, document_text_embedding, document_text_embedding)
-        document_image_text = self.image_text_pos_ffn(document_image_text)
+        # document_text_image, _ = self.text_image_attention(document_text_embedding, document_image_embedding, document_image_embedding)
+        # document_text_image = self.text_image_pos_ffn(document_text_image)
+        # document_image_text, _ = self.image_text_attention(document_image_embedding, document_text_embedding, document_text_embedding)
+        # document_image_text = self.image_text_pos_ffn(document_image_text)
 
-        claim_text_document_image, _ = self.text_image_attention(claim_text_embedding, document_image_embedding, document_image_embedding)
-        claim_text_document_image = self.text_image_pos_ffn(claim_text_document_image)
-        claim_image_document_text, _ = self.image_text_attention(claim_image_embedding, document_text_embedding, document_text_embedding)
-        claim_image_document_text = self.image_text_pos_ffn(claim_image_document_text)
+        # claim_text_document_image, _ = self.text_image_attention(claim_text_embedding, document_image_embedding, document_image_embedding)
+        # claim_text_document_image = self.text_image_pos_ffn(claim_text_document_image)
+        # claim_image_document_text, _ = self.image_text_attention(claim_image_embedding, document_text_embedding, document_text_embedding)
+        # claim_image_document_text = self.image_text_pos_ffn(claim_image_document_text)
 
-        document_image_claim_text, _ = self.claim_document_image_text_attention(document_image_embedding, claim_text_embedding, claim_text_embedding)
-        document_image_claim_text = self.claim_document_text_image_pos_ffn(document_image_claim_text)
-        document_text_claim_image, _ = self.claim_document_text_image_attention(document_text_embedding, claim_image_embedding, claim_image_embedding)
-        document_text_claim_image = self.claim_document_image_text_pos_ffn(document_text_claim_image)
+        # document_image_claim_text, _ = self.claim_document_image_text_attention(document_image_embedding, claim_text_embedding, claim_text_embedding)
+        # document_image_claim_text = self.claim_document_text_image_pos_ffn(document_image_claim_text)
+        # document_text_claim_image, _ = self.claim_document_text_image_attention(document_text_embedding, claim_image_embedding, claim_image_embedding)
+        # document_text_claim_image = self.claim_document_image_text_pos_ffn(document_text_claim_image)
 
         # aggregate word and image embedding to sentence embedding
         claim_document_text = torch.mean(claim_document_text, dim=1)
         document_claim_text = torch.mean(document_claim_text, dim=1)
-        claim_document_image = torch.mean(claim_document_image, dim=1)
-        document_claim_image = torch.mean(document_claim_image, dim=1)
+        # claim_document_image = torch.mean(claim_document_image, dim=1)
+        # document_claim_image = torch.mean(document_claim_image, dim=1)
         claim_text_embedding = torch.mean(claim_text_embedding, dim=1)
         document_text_embedding = torch.mean(document_text_embedding, dim=1)
-        claim_image_embedding = torch.mean(claim_image_embedding, dim=1)
-        document_image_embedding = torch.mean(document_image_embedding, dim=1)
-        claim_text_document_image = torch.mean(claim_text_document_image, dim=1)
-        claim_image_document_text = torch.mean(claim_image_document_text, dim=1)
-        document_image_claim_text = torch.mean(document_image_claim_text, dim=1)
-        document_text_claim_image = torch.mean(document_text_claim_image, dim=1)
+        # claim_image_embedding = torch.mean(claim_image_embedding, dim=1)
+        # document_image_embedding = torch.mean(document_image_embedding, dim=1)
+        # claim_text_document_image = torch.mean(claim_text_document_image, dim=1)
+        # claim_image_document_text = torch.mean(claim_image_document_text, dim=1)
+        # document_image_claim_text = torch.mean(document_image_claim_text, dim=1)
+        # document_text_claim_image = torch.mean(document_text_claim_image, dim=1)
 
-        claim_text_image = torch.mean(claim_text_image, dim=1)
-        claim_image_text = torch.mean(claim_image_text, dim=1)
-        document_text_image = torch.mean(document_text_image, dim=1)
-        document_image_text = torch.mean(document_image_text, dim=1)
+        # claim_text_image = torch.mean(claim_text_image, dim=1)
+        # claim_image_text = torch.mean(claim_image_text, dim=1)
+        # document_text_image = torch.mean(document_text_image, dim=1)
+        # document_image_text = torch.mean(document_image_text, dim=1)
 
         
-        concat_text_image_embeddings = torch.cat((claim_text_embedding, claim_image_embedding,
-                                       document_text_embedding, document_image_embedding,
-                                       claim_document_text, document_claim_text,
-                                       claim_document_image, document_claim_image,
-                                       claim_text_image, claim_image_text,
-                                       document_text_image, document_image_text,
-                                       claim_text_document_image, claim_image_document_text,
-                                       document_image_claim_text, document_text_claim_image), dim=-1)
+        concat_text_image_embeddings = torch.cat((
+            claim_text_embedding,
+            document_text_embedding,
+            claim_document_text, 
+            document_claim_text,
+        ), dim=-1)
 
         text_image_embeddings = self.attention_fusion(concat_text_image_embeddings)
-        feature_embeddings = self.feature_embedding(add_feature)
+        # feature_embeddings = self.feature_embedding(add_feature)
 
-        concat_embeddings = torch.cat((text_image_embeddings, feature_embeddings), dim=-1)
+        # concat_embeddings = torch.cat((text_image_embeddings, feature_embeddings), dim=-1)
 
-        predicted_output = self.classifier(concat_embeddings)
+        predicted_output = self.classifier(text_image_embeddings)
 
-        return predicted_output, concat_embeddings
+        return predicted_output, text_image_embeddings
--- train.py
+++ train.py
@@ -16,6 +16,7 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.models as models
 from torchvision import transforms
 from pytorch_metric_learning import losses
+import json
 
 from model import FakeNet
 
@@ -24,6 +25,11 @@ transformers_logger = logging.getLogger("transformers")
 transformers_logger.setLevel(logging.ERROR)
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+labels_dict = {
+    "Support": 0,
+    "Neutral": 1,
+    "Refute": 2,
+}
 
 def get_argument():
     opt = argparse.ArgumentParser()
@@ -47,34 +53,44 @@ class MultiModalDataset(Dataset):
     def __init__(self, mode='train'):
         super().__init__()
 
-        with open('../data/processed_{}.pickle'.format(mode), 'rb') as f:
-            self.data = pickle.load(f)
+        # with open('../data/processed_{}.pickle'.format(mode), 'rb') as f:
+        #     self.data = pickle.load(f)
+        
+        data_path = f"./data/{mode}.json"
+        print(f"Load data from {data_path}...")
+        with open(f"./data/{mode}.json", 'r') as f:
+            self.data = json.load(f)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         # + 1 for 2022 data (not sure why 2023 not need)
-        claim_text, claim_image, document_text, document_image, category, claim_ocr, document_ocr, add_feature = self.data[idx]
-        return (claim_text, claim_image, document_text, document_image, torch.tensor(category), claim_ocr, document_ocr, add_feature)
+        # print("data:", self.data[idx])
+        # print("self.data[idx].values:", self.data[idx].values)
+        # claim_text, claim_image, document_text, document_image, category, claim_ocr, document_ocr, add_feature = self.data[idx]
+        id, claim_id, claim, evidence, questions, claim_answers, evidence_answers, label = self.data[idx]["id"], self.data[idx]["claim_id"], self.data[idx]["claim"], self.data[idx]["evidence"], self.data[idx]["question"], self.data[idx]["claim_answer"], self.data[idx]["evidence_answer"], self.data[idx]["label"]
+        
+        # return (claim_text, claim_image, document_text, document_image, torch.tensor(category), claim_ocr, document_ocr, add_feature)
+        return (id, claim_id, claim, evidence, questions, claim_answers, evidence_answers, labels_dict[label])
 
 
-def save(model, vit_model, config, epoch=None):
+def save(model, config, epoch=None):
     output_folder_name = config['output_folder_name']
     if not os.path.exists(output_folder_name):
         os.makedirs(output_folder_name)
 
     if epoch is None:
         model_name = output_folder_name + 'model'
-        vit_model_name = output_folder_name + 'vitmodel'
+        # vit_model_name = output_folder_name + 'vitmodel'
         config_name = output_folder_name + 'config'
     else:
         model_name = output_folder_name + str(epoch) + 'model'
-        vit_model_name = output_folder_name + str(epoch) + 'vitmodel'
+        # vit_model_name = output_folder_name + str(epoch) + 'vitmodel'
         config_name = output_folder_name + str(epoch) + 'config'
     
     torch.save(model.state_dict(), model_name)
-    torch.save(vit_model.state_dict(), vit_model_name)
+    # torch.save(vit_model.state_dict(), vit_model_name)
     with open(config_name, 'w') as config_file:
         config_file.write(str(config))
 
@@ -96,38 +112,39 @@ if __name__ == '__main__':
             # if 'adapter' not in name:
             #     param.requires_grad = False
 
-    vit_model = Swinv2Model.from_pretrained(config['pretrained_image'])
-    if config['freeze_image']:
-        for name, param in vit_model.named_parameters():
-            if 'adapter' not in name:
-                param.requires_grad = False
+    # vit_model = Swinv2Model.from_pretrained(config['pretrained_image'])
+    # if config['freeze_image']:
+    #     for name, param in vit_model.named_parameters():
+    #         if 'adapter' not in name:
+    #             param.requires_grad = False
 
     fake_net = FakeNet(config)
 
-    fake_net.load_state_dict(torch.load('./model/20221201-131212_/10model', map_location=torch.device(f"cuda:{config['device']}")))
-    vit_model.load_state_dict(torch.load('./model/20221201-131212_/10vitmodel', map_location=torch.device(f"cuda:{config['device']}")))
+    # fake_net.load_state_dict(torch.load('./model/20221201-131212_/10model', map_location=torch.device(f"cuda:{config['device']}")))
+    # vit_model.load_state_dict(torch.load('./model/20221201-131212_/10vitmodel', map_location=torch.device(f"cuda:{config['device']}")))
 
     criterion = torch.nn.CrossEntropyLoss()
     fake_net_optimizer = AdamW(fake_net.parameters(), lr=config['lr'])
 
     device = torch.device(f"cuda:{config['device']}" if torch.cuda.is_available() else "cpu")
+    print("device:", device)
     # device = torch.device("cpu")
     # loss_func = losses.SupConLoss().to(device)
 
     deberta.to(device)
-    vit_model.to(device)
+    # vit_model.to(device)
     fake_net.to(device)
     criterion.to(device)
 
     train_dataset = MultiModalDataset(mode='train')
-    train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True, num_workers=8)
+    train_dataloader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True, num_workers=8, collate_fn=lambda x: tuple(zip(*x)))
     val_dataset = MultiModalDataset(mode='val')
-    val_dataloader = DataLoader(val_dataset, batch_size=8, shuffle=False, num_workers=8)
+    val_dataloader = DataLoader(val_dataset, batch_size=8, shuffle=False, num_workers=8, collate_fn=lambda x: tuple(zip(*x)))
 
     scheduler = get_scheduler("linear", fake_net_optimizer, num_warmup_steps=int(config['epochs']*len(train_dataloader)*0.1), num_training_steps=config['epochs']*len(train_dataloader))
 
     print(f"{sum(p.numel() for p in deberta.parameters() if p.requires_grad)}")
-    print(f"{sum(p.numel() for p in vit_model.parameters() if p.requires_grad)}")
+    # print(f"{sum(p.numel() for p in vit_model.parameters() if p.requires_grad)}")
     print(f"{sum(p.numel() for p in fake_net.parameters() if p.requires_grad)}")
 
     # training
@@ -137,27 +154,48 @@ if __name__ == '__main__':
         total_loss, best_val_f1, total_ce, total_scl = 0, 0, 0, 0
         for loader_idx, item in enumerate(train_dataloader): 
             fake_net_optimizer.zero_grad()
-            claim_text, claim_image, document_text, document_image, label, claim_ocr, document_ocr, add_feature = list(item[0]), item[1].to(device), list(item[2]), item[3].to(device), item[4].to(device), list(item[5]), list(item[6]), item[7].to(device)
-
+            # claim_text, claim_image, document_text, document_image, label, claim_ocr, document_ocr, add_feature = list(item[0]), item[1].to(device), list(item[2]), item[3].to(device), item[4].to(device), list(item[5]), list(item[6]), item[7].to(device)
+            claim_text, evidence_text, questions, claim_answers, evidence_answers, labels = list(item[2]), list(item[3]), list(item[4]), list(item[5]), list(item[6]), torch.tensor(item[7]).to(device)
+            
+            # question + answer
+            # claim_questions_answers = list()
+            # evidence_questions_answers = list()
+            # for i in range(question):
+            #     question = questions[i]
+            #     claim_answer = claim_answers[i]
+            #     evidence_answer = evidence_answers[i]
+            #     claim_questions_answers_str = ""
+            #     evidence_questions_answers_str = ""
+            #     for j in range(question):
+            #         if j == 0:
+            #             claim_questions_answers_str += question[j] + claim_answer[j]
+            #             evidence_questions_answers_str += question[j] + evidence_answer[j]
+            #         else:
+            #             claim_questions_answers_str += "[SEP]" + question[j] + claim_answer[j]
+            #             evidence_questions_answers_str += "[SEP]" + question[j] + evidence_answer[j]
+                        
+            #     claim_questions_answers.append(claim_questions_answers_str)
+            #     evidence_questions_answers.append(evidence_questions_answers_str)
+                
             # transform sentences to embeddings via DeBERTa
             input_claim = deberta_tokenizer(claim_text, truncation=True, padding=True, return_tensors="pt", max_length=config['max_sequence_length']).to(device)
             output_claim_text = deberta(**input_claim).last_hidden_state
 
-            input_document = deberta_tokenizer(document_text, truncation=True, padding=True, return_tensors="pt", max_length=config['max_sequence_length']).to(device)
-            output_document_text = deberta(**input_document).last_hidden_state
+            input_evidence = deberta_tokenizer(evidence_text, truncation=True, padding=True, return_tensors="pt", max_length=config['max_sequence_length']).to(device)
+            output_evidence_text = deberta(**input_evidence).last_hidden_state
 
-            input_claim_ocr = deberta_tokenizer(claim_ocr, truncation=True, padding=True, return_tensors="pt", max_length=config['max_sequence_length']).to(device)
-            output_claim_ocr = deberta(**input_claim_ocr).last_hidden_state
+            # input_claim_ocr = deberta_tokenizer(claim_ocr, truncation=True, padding=True, return_tensors="pt", max_length=config['max_sequence_length']).to(device)
+            # output_claim_ocr = deberta(**input_claim_ocr).last_hidden_state
 
-            input_document_ocr = deberta_tokenizer(document_ocr, truncation=True, padding=True, return_tensors="pt", max_length=config['max_sequence_length']).to(device)
-            output_document_ocr = deberta(**input_document_ocr).last_hidden_state
+            # input_document_ocr = deberta_tokenizer(document_ocr, truncation=True, padding=True, return_tensors="pt", max_length=config['max_sequence_length']).to(device)
+            # output_document_ocr = deberta(**input_document_ocr).last_hidden_state
 
-            output_claim_image = vit_model(claim_image).last_hidden_state
-            output_document_image = vit_model(document_image).last_hidden_state
+            # output_claim_image = vit_model(claim_image).last_hidden_state
+            # output_document_image = vit_model(document_image).last_hidden_state
 
-            predicted_output, concat_embeddings = fake_net(output_claim_text, output_claim_image, output_document_text, output_document_image, add_feature)
+            predicted_output, concat_embeddings = fake_net(output_claim_text, output_evidence_text)
             
-            ce_loss = criterion(predicted_output, label)
+            ce_loss = criterion(predicted_output, labels)
             # scl_loss = loss_func(concat_embeddings, label)
             # loss = config['loss_weight'] * ce_loss + (1 - config['loss_weight']) * scl_loss
             loss = ce_loss
@@ -170,59 +208,67 @@ if __name__ == '__main__':
             total_ce += ce_loss.item()
             # total_scl += scl_loss.item()
 
-            pbar.set_description("Loss: {}".format(round(current_loss, 3)), refresh=True)
+            pbar.set_description(f"Loss: {round(current_loss, 3)}", refresh=True)
 
             # if loader_idx == 2:
             #     break
 
         # print(f'total loss: {round(total_loss/len(train_dataloader), 4)} | ce: {round(total_ce/len(train_dataloader), 4)} | scl: {round(total_scl/len(train_dataloader), 4)}')
-        print(f'total loss: {round(total_loss/len(train_dataloader), 4)} | ce: {round(total_ce/len(train_dataloader), 4)}')
+        print(f'Total loss: {round(total_loss/len(train_dataloader), 4)}')
 
-        del claim_text, claim_image, document_text, document_image, label,  claim_ocr, document_ocr, add_feature, input_claim, output_claim_text, input_document, output_document_text, output_claim_image, output_document_image, predicted_output, loss
+        del claim_text, evidence_text, questions, claim_answers, evidence_answers, labels, input_claim, output_claim_text, input_evidence, output_evidence_text, predicted_output, loss
         gc.collect()
         with torch.cuda.device(f"cuda:{config['device']}"):
             torch.cuda.empty_cache()
-        save(fake_net, vit_model, config, epoch=epoch)
+        # save(fake_net, vit_model, config, epoch=epoch)
+        save(fake_net, config, epoch=epoch)
 
         if epoch % config['eval_per_epochs'] == 0:
             # testing
             with torch.no_grad():
                 y_pred, y_true = [], []
-                fake_net.eval(), deberta.eval(), vit_model.eval()
+                fake_net.eval(), deberta.eval()
                 for loader_idx, item in tqdm(enumerate(val_dataloader), total=len(val_dataloader)):
-                    claim_text, claim_image, document_text, document_image, label, claim_ocr, document_ocr, add_feature = list(item[0]), item[1].to(device), list(item[2]), item[3].to(device), item[4].to(device), list(item[5]), list(item[6]), item[7].to(device)
+                    # claim_text, claim_image, document_text, document_image, label, claim_ocr, document_ocr, add_feature = list(item[0]), item[1].to(device), list(item[2]), item[3].to(device), item[4].to(device), list(item[5]), list(item[6]), item[7].to(device)
+                    claim_text, evidence_text, questions, claim_answers, evidence_answers, labels = list(item[2]), list(item[3]), list(item[4]), list(item[5]), list(item[6]), torch.tensor(item[7]).to(device)
+
+                    # question + answer
+                    # claim_questions_answers = [[]*4]
+                    # evidence_questions_answers = [[]*4]
+                    # for i in range(len(questions)):
+                    #     for j in range(len(questions[i])):
+                    #         claim_questions_answers[j].append(questions[i][j] + claim_answers[i][j])
+                    #         evidence_questions_answers[j].append(questions[i][j] + evidence_answers[i][j])
 
                     # transform sentences to embeddings via DeBERTa
-                    input_claim = deberta_tokenizer(claim_text, truncation=True, padding=True, return_tensors="pt").to(device)
+                    input_claim = deberta_tokenizer(claim_text, truncation=True, padding=True, return_tensors="pt", max_length=config['max_sequence_length']).to(device)
                     output_claim_text = deberta(**input_claim).last_hidden_state
 
-                    input_document = deberta_tokenizer(document_text, truncation=True, padding=True, return_tensors="pt").to(device)
-                    output_document_text = deberta(**input_document).last_hidden_state
+                    input_evidence = deberta_tokenizer(evidence_text, truncation=True, padding=True, return_tensors="pt", max_length=config['max_sequence_length']).to(device)
+                    output_evidence_text = deberta(**input_evidence).last_hidden_state
 
-                    output_claim_image = vit_model(claim_image).last_hidden_state
-                    output_document_image = vit_model(document_image).last_hidden_state
-
-                    predicted_output, concat_embeddings = fake_net(output_claim_text, output_claim_image, output_document_text, output_document_image, add_feature)
-                    
-                    _, predicted_label = torch.topk(predicted_output, 1)
+                    predicted_output, concat_embeddings = fake_net(output_claim_text, output_evidence_text) 
+                                       
+                    _, predicted_labels = torch.topk(predicted_output, 1)
 
                     if len(y_pred) == 0:
-                        y_pred = predicted_label.cpu().detach().flatten().tolist()
-                        y_true = label.tolist()
+                        y_pred = predicted_labels.cpu().detach().flatten().tolist()
+                        y_true = labels.tolist()
                     else:
-                        y_pred += predicted_label.cpu().detach().flatten().tolist()
-                        y_true += label.tolist()
+                        y_pred += predicted_labels.cpu().detach().flatten().tolist()
+                        y_true += labels.tolist()
 
                 f1 = round(f1_score(y_true, y_pred, average='weighted'), 5)
 
                 if f1 >= best_val_f1:
                     best_val_f1 = f1
-                    save(fake_net, vit_model, config, epoch=epoch)
+                    save(fake_net, config, epoch=epoch)
 
+                print(f"Epoch: {epoch}, Total loss: {round(total_loss/len(train_dataloader), 5)}, F1: {f1}")
                 with open(config['output_folder_name'] + 'record', 'a') as config_file:
                     config_file.write(str(epoch) + ',' + str(round(total_loss/len(train_dataloader), 5)) + ',' + str(f1))
                     config_file.write('\n')
 
     config['total_loss'] = total_loss
     config['val_f1'] = best_val_f1
-    save(fake_net, vit_model, config)
+    save(fake_net, config)
