fdb4b823dcfabb02dca0c654d9d15f1a12960b5f
AndyChiangSH <andy10801@gmail.com>
2023-11-07 00:31:11 +0800

    Train 20231106-220340_gpt2

--- train.py
+++ train.py
@@ -318,6 +318,8 @@ if __name__ == '__main__':
                 print(f"[Val] epoch: {epoch}, total loss: {round(total_loss/len(train_dataloader), 5)}, F1: {f1}, accuracy: {accuracy}")
                 
                 # save model and record
+                config['val_f1'] = float(f1)
+                config['val_accurancy'] = float(accuracy)
                 save(fake_net, config, epoch=epoch)
                 with open(config['output_folder_name'] + 'record.csv', 'a') as record_file:
                     record_file.write(f"{epoch},{round(total_loss/len(train_dataloader), 5)},{f1},{accuracy}\n")
